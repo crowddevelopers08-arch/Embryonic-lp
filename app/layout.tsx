7106178d031a0420ebf7b04b96c3f6877a50fd47
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-09-24 18:01:29 +0530

    gtags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,26 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={`${lexend.variable} scroll-smooth`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W92Z5PSP');
+            `,
+          }}
+        />
+
         {/* Google Ads (gtag.js) */}
         <Script
           id="google-ads"
@@ -99,6 +119,16 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         </Script>
       </head>
       <body className="min-h-screen bg-white font-[family-name:var(--font-lexend)] text-[#2c1d27] antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W92Z5PSP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {/* Meta Pixel Noscript Fallback */}
         <noscript>
           <img
